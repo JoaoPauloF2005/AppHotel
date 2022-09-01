23c1e1618bf6a4db1187fc8f7f6b821bdde8b881
Usuario <Usuario@CPD03-M04>
2022-09-01 14:08:16 -0300

    Adicionar arquivos de projeto.

---
+++ AppHotel/AppHotel/Model/Suite.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppHotel.Model
+{
+    public class Suite
+    {
+        public string Descricao { get; set; }
+
+        public double ValorDiariaAdulto { get; set; }
+
+        public double ValorDiariaCrianca { get; set; }
+
+    }
+}
